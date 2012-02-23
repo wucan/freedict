34723a6f464f4a022dd66054a086c53206d9a6ab
Can Wu <wu.canus@gmail.com>
2012-02-23 17:56:30 +0800

    add api to get string lower/upper/mix feature

---
+++ src/fd-str.c
@@ -0,0 +1,49 @@
+#include <glib.h>
+
+#include "fd-str.h"
+
+
+gboolean fd_str_is_all_upper(gchar *s)
+{
+	while (*s) {
+		if (g_ascii_islower(*s))
+			return FALSE;
+		s++;
+	}
+
+	return TRUE;
+}
+
+gboolean fd_str_is_all_lower(gchar *s)
+{
+	while (*s) {
+		if (g_ascii_isupper(*s))
+			return FALSE;
+		s++;
+	}
+
+	return TRUE;
+}
+
+gint fd_str_get_feature(gchar *s)
+{
+	int check_flags = 0;
+
+	while (*s) {
+		if (g_ascii_islower(*s))
+			check_flags |= (1 << 0);
+		if (g_ascii_isupper(*s))
+			check_flags |= (1 << 1);
+		if (check_flags == (1 << 1) | (1 << 0))
+			break;
+		s++;
+	}
+
+	return check_flags;
+}
+
+gboolean fd_str_is_mix(gchar *s)
+{
+	return (fd_str_get_feature(s) == FD_STR_FEATURE_MIX);
+}
+
---
+++ src/fd-str.h
@@ -0,0 +1,19 @@
+#ifndef _FD_STR_H_
+#define _FD_STR_H_
+
+
+/*
+ * string feature: all lower, all upper, mix
+ */
+#define FD_STR_FEATURE_ALL_LOWER		(1 << 0)
+#define FD_STR_FEATURE_ALL_UPPER		(1 << 1)
+#define FD_STR_FEATURE_MIX				((1 << 0) | (1 << 1))
+
+gboolean fd_str_is_all_upper(gchar *s);
+gboolean fd_str_is_all_lower(gchar *s);
+gboolean fd_str_is_mix(gchar *s);
+gint fd_str_get_feature(gchar *s);
+
+
+#endif /* _FD_STR_H_ */
+
