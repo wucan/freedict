5e998c28d2fe9eb23246068263dd8ed8e7230b59
Can Wu <wu.canus@gmail.com>
2012-02-24 11:16:49 +0800

    utils: add function to get window from name

--- src/fd-utils.c
+++ src/fd-utils.c
@@ -4,6 +4,9 @@
 #include <glib.h>
 
 
+static Window Window_With_Name(Display *dpy, Window top, const char *name);
+static Window find_window_with_name(Display *display, const char *name);
+
 gchar * fd_utils_get_active_window_title()
 {
 	Status status;
@@ -49,5 +52,44 @@ done:
 		window_name = g_strdup("Unknown Context!");
 
 	return window_name;
+}
+
+/*
+ * Routine steal form xprop.
+ *
+ * Window_With_Name: routine to locate a window with a given name on a display.
+ *									 If no window with the given name is found, 0 is returned.
+ *									 If more than one window has the given name, the first
+ *									 one found will be returned.	Only top and its subwindows
+ *									 are looked at.	Normally, top should be the RootWindow.
+ */
+static Window Window_With_Name(Display *dpy, Window top, const char *name)
+{
+	Window *children, dummy;
+	unsigned int nchildren;
+	int i;
+	Window w=0;
+	char *window_name;
+
+	if (XFetchName(dpy, top, &window_name) && !strcmp(window_name, name))
+		return(top);
+
+	if (!XQueryTree(dpy, top, &dummy, &dummy, &children, &nchildren))
+		return(0);
+
+	for (i=0; i<nchildren; i++) {
+		w = Window_With_Name(dpy, children[i], name);
+		if (w)
+			break;
+	}
+
+	if (children) XFree((char *)children);
+
+	return(w);
+}
+
+static Window find_window_with_name(Display *display, const char *name)
+{
+	return Window_With_Name(display, RootWindow(display, 0), name);
 }
 
