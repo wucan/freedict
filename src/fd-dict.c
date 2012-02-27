ea8f9ad3e741aaf962f51417c01fcf579cfd6143
Can Wu <wu.canus@gmail.com>
2012-02-27 15:49:46 +0800

    dict: index dict data to accelerate locating

--- src/fd-dict.c
+++ src/fd-dict.c
@@ -1,6 +1,7 @@
 #include <glib.h>
 
 #include "fd-common.h"
+#include "fd-str.h"
 
 
 struct fd_dict_box {
@@ -10,6 +11,12 @@ struct fd_dict_box {
 
 static struct fd_dict_box dict_box;
 static struct fd_dict *cur_dict;
+
+struct dict_indexer {
+	gchar *head, *tail;
+};
+
+static struct dict_indexer indexer[0x80];
 
 static gchar * do_conv(gchar *in_encode, gchar *in, gsize in_size)
 {
@@ -32,20 +39,51 @@ static gchar * do_conv(gchar *in_encode, gchar *in, gsize in_size)
 	return out_head;
 }
 
+static void dict_build_index(struct fd_dict *dict)
+{
+	gchar *lines = dict->data;
+	gchar *line;
+	gchar c, last_c = 0;
+
+	while (line = fd_str_next_line_pointer(&lines)) {
+		c = line[0];
+
+		if (c != last_c) {
+			indexer[c].head = line;
+			if (indexer[last_c].head)
+				indexer[last_c].tail = line - 1;
+			last_c = c;
+		}
+	}
+
+	if (indexer[last_c].head)
+		indexer[last_c].tail = lines;
+}
+
 static gchar * get_answer(const char *words)
 {
 	GRegex *reg;
 	GMatchInfo *match_info;
 	gint start = -1, end = -1;
 	gchar pat[1024];
+	gchar *data_head, *data_tail;
 
-	sprintf(pat, "\n%s\t", words);
+	if ((guchar)words[0] >= 0x80 || !indexer[words[0]].head) {
+		return NULL;
+	}
+
+	sprintf(pat, "%s\t", words);
 	reg = g_regex_new(pat, 0, 0, NULL);
 	if (!reg) {
 		g_print("failed to create regex! words: %s\n", words);
 		return NULL;
 	}
-	g_regex_match(reg, cur_dict->data, 0, &match_info);
+
+	data_head = indexer[words[0]].head;
+	data_tail = indexer[words[0]].tail;
+	*data_tail = 0;
+
+	g_regex_match(reg, data_head, 0, &match_info);
 	while (g_match_info_matches(match_info)) {
 		gchar *word = g_match_info_fetch(match_info, 0);
 		g_match_info_fetch_pos(match_info, 0, &start, &end);
@@ -58,11 +96,13 @@ static gchar * get_answer(const char *words)
 	g_match_info_free(match_info);
 	g_regex_unref(reg);
 
+	*data_tail = '\n';
+
 	if (start > 0 && end > 0) {
 		int tail;
-		for (tail = end; cur_dict->data[tail] != '\n'; tail++)
+		for (tail = end; data_head[tail] != '\n'; tail++)
 			;
-		return do_conv(cur_dict->char_encode, &cur_dict->data[end], tail - end);
+		return do_conv(cur_dict->char_encode, &data_head[end], tail - end);
 	}
 
 	return NULL;
@@ -98,6 +138,8 @@ static struct fd_dict * fd_dict_load_dict(gchar *uri)
 		g_free(dict);
 		return NULL;
 	}
+
+	dict_build_index(dict);
 
 	/*
 	 * prepare the dictionry access method, and preparse for quick search?
